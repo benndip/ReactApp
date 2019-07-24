c023412a7fb7affbec9ac33c7472ea07b64f8b91
benndip <obentabiayuk1@gmail.com>
2019-07-24 01:22:14 +0100

    added icon

--- App.js
+++ App.js
@@ -10,22 +10,32 @@ import{
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App extends React.Component{
+
+  constructor(Props){
+    super(Props);
+
+    this.state={
+
+    }
+  }
+
   render(){
     return(
       <View style={styles.container}>
-        <View style={styles.text}>
-        <Text>Welcome To Benndip's Tic-Tac-Toe</Text>
-        </View>
-       
+      
         <View style={{flexDirection:'row'}}>
        <TouchableOpacity>
          <View style={[styles.tile,{borderLeftWidth:0,borderTopWidth:0}]}>
-         <Icon name="circle-thin" size={60} color="#900" />
+         <Icon name="close" style={styles.tilex} />
          </View>
        </TouchableOpacity>
 
 
-       <TouchableOpacity><View style={[styles.tile,{borderTopWidth:0}]}></View></TouchableOpacity>
+       <TouchableOpacity>
+         <View style={[styles.tile,{borderTopWidth:0}]}>
+         <Icon name="circle-o" style={styles.tileO} />
+         </View>
+         </TouchableOpacity>
        <TouchableOpacity><View style={[styles.tile,{borderRightWidth:0,borderTopWidth:0}]}></View></TouchableOpacity>
         </View>
 
@@ -62,5 +72,17 @@ const styles=StyleSheet.create({
   backgroundColor:'aquamarine',
  marginVertical:20,
  paddingTop:10
+  },
+  tilex:{
+    color:"red",
+    fontSize:60,
+    marginLeft:16,
+    marginVertical:13
+  },
+  tileO:{
+    color:"green",
+    fontSize:60,
+    marginLeft:15,
+    marginVertical:13
   }
 })
