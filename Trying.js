50fa4feb2c7bef1a027b4bf64849f3c5a538b20e
benndip <obentabiayuk1@gmail.com>
2019-08-03 03:20:19 +0100

    okooo

---
+++ Trying.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+export default class Trying extends React.Component{
+ render(){
+     return(
+         <View>
+             <Text>Hello Benndip</Text>
+             </View>
+     );
+ }
+};
--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Trying from './Trying';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Trying);
